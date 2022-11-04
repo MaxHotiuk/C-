a58e08e22b691f7622621655cd5347b1035d3f58
MaxHotiuk <103311303+MaxHotiuk@users.noreply.github.com>
2022-11-04 15:54:41 +0200

    new file

--- homework_programming/004/1.cpp
+++ homework_programming/004/1.cpp
@@ -5,35 +5,28 @@ using namespace std;
 
 int main()
 {
-    int count, indx1, indx2, if1 = 0, if2 = 0;
+    int count, indx1 = 0, indx2 = 0, if2 = 0;
 
     cout << "Input count of elements: ";
     cin >> count;
 
-    double *arr = new double(count), el1, el2, sum;
+    double *arr = new double(count), sum;
 
     for (int x = 0; x < count; x++){
         cout << endl << "Input element: ";
         cin >> arr[x];
     }
+    double el1 = arr[0], el2 = arr[0];
 
     for (int x = 0; x < count; x++){
-        if (if1 == 0){
-            if1 = 1;
-            el1 = arr[x];
-            indx1 = x;
-        } else if (arr[x] > el1){
+        if (arr[x] > el1){
             el1 = arr[x];
             indx1 = x;
         }
     }
 
     for (int x = 0; x < count; x++){
-        if (if2 == 0){
-            if2 = 1;
-            el2 = arr[x];
-            indx2 = x;
-        } else if (arr[x] < el2){
+        if (arr[x] < el2){
             el2 = arr[x];
             indx2 = x;
         }
@@ -45,4 +38,3 @@ int main()
 
     cout << sum;
 }
-
--- homework_programming/004/3.cpp
+++ homework_programming/004/3.cpp
@@ -10,30 +10,7 @@ int main()
     cout << "Input count of elements: ";
     cin >> count;
 
-    double **arr = new double*[count], dot1[2], dot2[2], iff = 0, indx1, indx2, len;
-    for(int x = 0; x < count; x++){
-        arr[x] = new double[2];
-    }
-
-    for (int x = 0; x < count; x++){
-        cout << endl << "Input integer element: ";
-        cin >> arr[x][0];
-        cout << " ";
-        cin >> arr[x][1];
-    }
-
-    for (int x = 0; x < count; x++){
-        for (int y = 0; y < 2; y++){
-            if (iff == 0){
-                iff = 1;
-                dot1[0] = x;
-                dot1[1] = y;
-                
-            } else {
-
-            }
-        }
-    }
+    double *arrx = new double [count], *arry = new double [count];
     
 }
 
